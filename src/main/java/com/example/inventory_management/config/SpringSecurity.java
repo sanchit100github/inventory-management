3a76d98acf7361798393265b0f90000d2d94d6ad
Sanchit <sanchitkhad100@gmail.com>
2025-04-14 14:35:32 +0530

    5173 commit

--- src/main/java/com/example/inventory_management/config/SpringSecurity.java
+++ src/main/java/com/example/inventory_management/config/SpringSecurity.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -45,8 +44,6 @@ public class SpringSecurity {
                         .permitAll()
                         .requestMatchers("/test1").authenticated()
                         .anyRequest().permitAll())
-                // .addFilterBefore(roleAuthorizationFilter,
-                // UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
@@ -62,7 +59,7 @@ public class SpringSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // or any frontend domain
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // or any frontend domain
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Important if using cookies or sessions
